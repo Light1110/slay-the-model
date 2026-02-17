27acfcde18448d6c5f25474cdf64379a36aec8da
Kunzhen Wu <wkzmagician@gmail.com>
2026-02-17 12:48:31 +0800

    fix: add dest_pile parameter to BuyItemAction AddCardAction call
    AddCardAction now requires dest_pile parameter. Shop purchases should
    add cards to the discard pile, matching Slay the Spire behavior.

--- actions/misc.py
+++ actions/misc.py
@@ -111,7 +111,7 @@ class BuyItemAction(Action):
         game_state.player.gold -= final_price
 
         if self.shop_item.item_type == "card":
-            AddCardAction(card=self.shop_item.item).execute()
+            AddCardAction(card=self.shop_item.item, dest_pile="discard", source="shop").execute()
         elif self.shop_item.item_type == "relic":
             AddRelicAction(relic=self.shop_item.item.idstr).execute()
         elif self.shop_item.item_type == "potion":
